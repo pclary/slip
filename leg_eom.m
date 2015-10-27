4665edca5a24e73511248e641a5bbcece5972d5f
Patrick <pclary@outlook.com>
2015-10-27 11:35:54 -0700

    leg_eom.m calculates nearly complete set of EOMs

--- leg_eom.m
+++ leg_eom.m
@@ -51,34 +51,64 @@ thbddot = simplify(thbddot)
 thoddotrel = simplify(thoddotrel)
 
 %% Two gearboxes in the same body
-syms ddthma ddthoa ddthmb ddthob ddthb Im Ib Ioa Iob Tma Tmb Tb Toa Tint1a Tint2a Tbb Tob Tint1b Tint2b n
+syms ddthb ddthoa ddthob ddthma ddthmb Tint1a Tint2a Tint1b Tint2b ddbx ddby ddla ddlb
+syms Tma Tmb Tb Fexa Feya Fexb Feyb
+syms Im Ib b n lxa lya lxb lyb mb mf la lb dthma dthmb dthla dthlb dthb
 % motor and output angles are relative to body
 
 ddthoaabs = ddthb + ddthoa;
 ddthobabs = ddthb + ddthob;
 ddthmaabs = ddthb + ddthma;
 ddthmbabs = ddthb + ddthmb;
+Foa = -lya*(Fexa - mf*ddbx) + lxa*(Feya - mf*ddby);
+Fob = -lyb*(Fexb - mf*ddbx) + lxb*(Feyb - mf*ddby);
+Fbx = -lya*Foa - lyb*Fob;
+Fby = lxa*Foa + lxb*Fob;
+Toa = Foa*la;
+Tob = Fob*lb;
+Ioa = la^2*mf;
+Iob = lb^2*mf;
+Tmanet = Tma - b*dthma;
+Tmbnet = Tmb - b*dthmb;
+Fla = lxa*(Fexa - mf*ddbx) + lya*(Feya - mf*ddby) - mf*la*(dthla + dthb)^2;
+Flb = lxb*(Fexb - mf*ddbx) + lyb*(Feyb - mf*ddby) - mf*lb*(dthlb + dthb)^2;
 
-eq1a = Im*ddthmaabs - (Tma - Tint1a);
+eq1a = Im*ddthmaabs - (Tmanet - Tint1a);
 eq2a = Ioa*ddthoaabs - (Toa + Tint2a);
 eq3a = ddthma - n*ddthoa;
 eq4a = Tint2a - n*Tint1a;
-eq1b = Im*ddthmbabs - (Tmb - Tint1b);
+eq1b = Im*ddthmbabs - (Tmbnet - Tint1b);
 eq2b = Iob*ddthobabs - (Tob + Tint2b);
 eq3b = ddthmb - n*ddthob;
 eq4b = Tint2b - n*Tint1b;
-eq5 = Ib*ddthb - (Tb - Tma - Tmb + Tint1a + Tint1b - Tint2a - Tint2b);
+eq5 = Ib*ddthb - (Tb - Tmanet - Tmbnet + Tint1a + Tint1b - Tint2a - Tint2b);
+eq6 = mb*ddbx - Fbx;
+eq7 = mb*ddby - Fby;
+eq8a = mf*ddla - Fla;
+eq8b = mf*ddlb - Flb;
 
-[ddthb, ddthoa, ddthob, ddthma, ddthmb, Tint1a, Tint1b, Tint2a, Tint2b] = ...
-    solve(eq1a, eq2a, eq3a, eq4a, eq1b, eq2b, eq3b, eq4b, eq5, ...
-    ddthb, ddthoa, ddthob, ddthma, ddthmb, Tint1a, Tint1b, Tint2a, Tint2b);
+[ddthb, ddthoa, ddthob, ddthma, ddthmb, Tint1a, Tint2a, Tint1b, Tint2b, ddbx, ddby, ddla, ddlb] = ...
+    solve(eq1a, eq2a, eq3a, eq4a, eq1b, eq2b, eq3b, eq4b, eq5, eq6, eq7, eq8a, eq8b, ...
+    ddthb, ddthoa, ddthob, ddthma, ddthmb, Tint1a, Tint2a, Tint1b, Tint2b, ddbx, ddby, ddla, ddlb);
 
 ddthb = simplify(ddthb);
 ddthoa = simplify(ddthoa);
 ddthab = simplify(ddthob);
+ddbx = simplify(ddbx);
+ddby = simplify(ddby);
+ddla = simplify(ddla);
+ddlb = simplify(ddlb);
 
-syms Itot
-[~, Itotexpr] = numden(ddthb)
-ddthb = simplify(subs(ddthb, Itotexpr, Itot))
-ddthoa = simplify(subs(ddthoa, Itotexpr, Itot))
-ddthob = simplify(subs(ddthob, Itotexpr, Itot))
+syms Itot mtot
+[~, Itotexpr] = numden(ddthb);
+Itotexpr = collect(Itotexpr, n)
+[~, mbtotexpr] = numden(ddbx)
+ddthb = simplify(subs(ddthb, Itotexpr, Itot));
+ddthoa = simplify(subs(ddthoa, Itotexpr, Itot));
+ddthob = simplify(subs(ddthob, Itotexpr, Itot));
+ddbx = simplify(subs(ddbx, mbtotexpr, mtot));
+ddby = simplify(subs(ddby, mbtotexpr, mtot));
+
+J = jacobian([ddbx; ddby; ddthb; ddla; ddthoa; ddlb; ddthob], ...
+    [Tb; Fexa; Feya; Tma; Fexb; Feyb; Tmb;]);
+J = simplify(J)
