f0a5af072029d587616fbb1ce00c5cae69b3d8c8
Patrick Clary <pclary@outlook.com>
2015-10-30 20:41:35 -0700

    Replaced ode45 with custom simplified solver for compilation

--- find_eq_angle.m
+++ find_eq_angle.m
@@ -9,8 +9,7 @@ odefun = @(t, Y)[Y(2);
                  Y(1)*Y(4)^2 - g*cos(Y(3)) + k/m*(eq_length - Y(1));
                  Y(4);
                  (g*sin(Y(3)) - 2*Y(2)*Y(4))/Y(1)];
-eventfun = @(t, Y) takeoff_event(t, Y, eq_length);
-odeopts = odeset('Events', eventfun);
+stopfun = @(Y) eq_length - Y(1);
 
     function Y0 = initial_conditions(th0)
         l0 = eq_length;
@@ -24,16 +23,49 @@ odeopts = odeset('Events', eventfun);
 
     function out = zerofun(th0)
         Y0 = initial_conditions(th0);
-        [~, Y] = ode45(odefun, [0 1e2], Y0, odeopts);
-        out = th0 + Y(end, 3);
+        dtmax = 1e-2;
+        dtmin = 1e-3;
+        Y = odeshoot(odefun, Y0, dtmax, dtmin, stopfun, 1e2);
+        out = th0 + Y(3);
     end
 
 th_eq = fzero(@zerofun, 0);
 end
 
 
-function [value, isterminal, direction] = takeoff_event(~, Y, eq_length)
-value = [eq_length - Y(1); abs(Y(3)) - pi/2];
-isterminal = [1; 1];
-direction = [-1; 1];
+function Yend = odeshoot(odefun, Y0, dtmax, dtmin, stopfun, tstop)
+% stopfun(Y) should return >0 before stop condition, 0 at stop condition, 
+% and <0 after stop condition
+t = 0;
+dt = dtmax;
+Y = Y0;
+while t < tstop
+    Ynew = rk4step(Y, t, dt, odefun);
+    sfval = stopfun(Ynew);
+    if sfval <= 0
+        if dt/2 >= dtmin
+            dt = dt/2;
+            continue;
+        else
+            Y = Ynew;
+            break;
+        end
+    else
+        Y = Ynew;
+        t = t + dt;
+    end
+end
+Yend = Y;
+end
+
+
+function Ynew = rk4step(Y, t, dt, odefun)
+dY1 = odefun(t, Y);
+Y1 = Y + dt/2*dY1;
+dY2 = odefun(t + dt/2, Y1);
+Y2 = Y + dt/2*dY2;
+dY3 = odefun(t + dt/2, Y2);
+Y3 = Y + dt*dY3;
+dY4 = odefun(t + dt, Y3);
+Ynew = Y + dt/6*(dY1 + 2*dY2 + 2*dY3 + dY4);
 end
