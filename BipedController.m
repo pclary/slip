1fba6505cd8ee1a44135f28ef71232de8fffc0e6
Patrick Clary <pclary@outlook.com>
2016-08-26 15:47:54 -0700

    Basic one-step prediction

--- BipedController.m
+++ BipedController.m
@@ -1,11 +1,13 @@
 classdef BipedController < matlab.System & matlab.system.mixin.Propagates
     
     properties (Nontunable)
-        Ts = 1e-3
+        Ts = 1e-3;
+        env = Environment();
+        ground_data = zeros(1, 5);
     end
 
     properties
-        phase_rate = 1.6;
+        phase_rate = 1.5;
         target_dx = 0;
         step_offset = 0;
         energy_injection = 0;
@@ -14,6 +16,8 @@ classdef BipedController < matlab.System & matlab.system.mixin.Propagates
     
     properties (Access = private)
         cstate;
+        cparams;
+        cparams_default;
     end
     
     
@@ -22,23 +26,45 @@ classdef BipedController < matlab.System & matlab.system.mixin.Propagates
         end
         
         
-        function [u, dbg] = stepImpl(obj, t, X)
+        function u = stepImpl(obj, X)
             
-            cparams = ControllerParams();
-            cparams.phase_rate = obj.phase_rate;
-            cparams.target_dx = obj.target_dx;
-            cparams.step_offset = obj.step_offset;
-            cparams.energy_injection = obj.energy_injection;
+            obj.cparams_default.phase_rate = obj.phase_rate;
+            obj.cparams_default.target_dx = obj.target_dx;
+            obj.cparams_default.step_offset = obj.step_offset;
+            obj.cparams_default.energy_injection = obj.energy_injection;
+            
+            obj.cparams.phase_rate = 0.99*obj.cparams.phase_rate + 0.01*obj.cparams_default.phase_rate;
+            obj.cparams.target_dx = 0.99*obj.cparams.target_dx + 0.01*obj.cparams_default.target_dx;
+            obj.cparams.step_offset = 0.99*obj.cparams.step_offset + 0.01*obj.cparams_default.step_offset;
+            obj.cparams.energy_injection = 0.99*obj.cparams.energy_injection + 0.01*obj.cparams_default.energy_injection;
+            
+            step_time = 1 / obj.cparams.phase_rate / 2;
+            
+            Xpred = biped_sim(X, obj.cstate, obj.cparams, step_time, obj.Ts, obj.env, obj.ground_data);
+            
+            cparams_new = obj.cparams;
+            cparams_new.phase_rate = cparams_new.phase_rate + randn() * 0.1;
+            cparams_new.target_dx = cparams_new.target_dx + randn() * 0.3;
+            cparams_new.step_offset = cparams_new.step_offset + randn() * 0.1;
+            cparams_new.energy_injection = cparams_new.energy_injection + randn() * 100;
+            
+            
+            
+            Xpred_new = biped_sim(X, obj.cstate, cparams_new, step_time, obj.Ts, obj.env, obj.ground_data);
+            
+            if state_eval(Xpred_new) > state_eval(Xpred)
+                obj.cparams = cparams_new;
+            end
 
+            [u, obj.cstate] = controller_step(X, obj.cstate, obj.cparams, obj.Ts);
             
-            [u, obj.cstate] = controller_step(X, obj.cstate, cparams, obj.Ts);
-            
-            dbg = [0, 0];
         end
         
         
         function resetImpl(obj)
             obj.cstate = ControllerState();
+            obj.cparams = ControllerParams();
+            obj.cparams_default = ControllerParams();
         end
         
         
@@ -71,35 +97,11 @@ classdef BipedController < matlab.System & matlab.system.mixin.Propagates
 end
 
 
+function v = state_eval(X)
 
-function tvals = interp_trajectory(tpoints, phase)
+y_right = X.body.y - X.right.l * cos(X.body.theta + X.right.theta);
+y_left = X.body.y - X.left.l * cos(X.body.theta + X.left.theta);
 
-i = 2;
-while tpoints(i).phase < phase
-    i = i + 1;
+v = min(min(y_right, y_left), -1e-3);
+
 end
-
-phase_diff = tpoints(i).phase - tpoints(i - 1).phase;
-p = (phase - tpoints(i - 1).phase) / phase_diff;
-
-tvals.phase = phase;
-tvals.torque = tpoints(i - 1).torque + p * (tpoints(i).torque - tpoints(i - 1).torque);
-tvals.dtorque = (tpoints(i).torque - tpoints(i - 1).torque) / phase_diff;
-tvals.target = tpoints(i - 1).target + p * (tpoints(i).target - tpoints(i - 1).target);
-tvals.dtarget = (tpoints(i).target - tpoints(i - 1).target) / phase_diff;
-tvals.kp = tpoints(i - 1).kp + p * (tpoints(i).kp - tpoints(i - 1).kp);
-tvals.kd = tpoints(i - 1).kd + p * (tpoints(i).kd - tpoints(i - 1).kd);
-end
-
-
-function torque = eval_trajectory(tvals, x, dx)
-torque = tvals.torque + ...
-    (tvals.kp * (tvals.target - x)) + ...
-    (tvals.kd * (tvals.dtarget - dx));
-end
-
-
-function out = clamp(x, l, h)
-out = min(max(x, l), h);
-end
-        
--- ControllerParams.m
+++ ControllerParams.m
@@ -1,7 +1,7 @@
 function p = ControllerParams()
 
 p = struct();
-p.phase_rate = 1.6;
+p.phase_rate = 1.5;
 p.target_dx = 0;
 p.step_offset = 0;
 p.energy_injection = 0;
