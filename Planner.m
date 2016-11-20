9a8d7ca7a81bf5132d0296d4414edb622f7659b0
Patrick Clary <pclary@outlook.com>
2016-11-19 19:50:24 -0800

    Implemented a neural net policy

--- Planner.m
+++ Planner.m
@@ -20,6 +20,7 @@ classdef Planner < matlab.System & matlab.system.mixin.Propagates
         tree
         rollout_node
         env
+        cparams
     end
     
     
@@ -31,124 +32,125 @@ classdef Planner < matlab.System & matlab.system.mixin.Propagates
         
         
         function [cparams] = stepImpl(obj, X, cstate)
+            cparams = obj.cparams;
 %             cparams = ControllerParams();
 %             cparams.target_dx = obj.target_dx;
 %             cparams.energy_injection = obj.energy_injection;
 %             cparams.phase_rate = obj.phase_rate;
 %             cparams.max_stride = obj.max_stride;
 %             cparams.step_height = obj.step_height;
-            goal = Goal();
-            goal.dx = obj.target_dx;
-            
-            cparams = obj.tree.nodes(1).data.cparams;
-            
-            % Calculate leg phases at next planner timestep
-            phase_inc = obj.Ts * cparams.phase_rate;
-            phase_right_next = cstate.phase.right + phase_inc;
-            phase_left_next = cstate.phase.left + phase_inc;
-            
-            % Check whether next planner timestep will start a new step
-            if phase_right_next >= 1 || phase_left_next >= 1 || ...
-                    cstate.phase.right == 0 || cstate.phase.left == 0
-                % Project one planer timestep forward with current parameters
-                terrain = obj.env.getLocalTerrain(X.body.x);
-                [Xp, cstatep] = biped_sim(X, cstate, obj.robot, cparams, terrain, obj.Ts, obj.Ts_sim);
-                
-                % Choose the new parameters with the highest estimated Q value
-                v_max = -inf;
-                for i = 1:numel(obj.tree.nodes(1).children)
-                    c = obj.tree.nodes(1).children(i);
-                    if c
-                        v = obj.tree.nodes(c).data.value;
-                        if v > v_max
-                            cparams = obj.tree.nodes(c).data.cparams;
-                            v_max = v;
-                        end
-                    end
-                end
-                
-                % Simulate until the end of the next step
-                phase_next_max = max(mod(phase_right_next, 1), mod(phase_left_next, 1));
-                t_stop = (1 - phase_next_max) / cparams.phase_rate;
-                terrain = obj.env.getLocalTerrain(Xp.body.x);
-                [Xp, cstatep] = biped_sim(Xp, cstatep, obj.robot, cparams, terrain, t_stop, obj.Ts_sim);
-                
-                % Reset tree with predicted state as root
-                terrain = obj.env.getLocalTerrain(Xp.body.x);
-                vp = value(Xp, goal, terrain);
-                ss = SimulationState(Xp, cstatep, cparams, GeneratorState(), vp);
-                obj.tree.reset(ss);
-                obj.rollout_node = uint32(1);
-            else
-                % Otherwise, grow the tree
-                
-                % TODO: only pick nodes with no high-value child
-                
-                % Check whether max depth on current rollout has been reached
-                if obj.tree.nodes(obj.rollout_node).depth >= obj.rollout_depth
-                    % Evaluate leaf node
-                    Xn = obj.tree.nodes(obj.rollout_node).data.X;
-                    terrain = obj.env.getLocalTerrain(Xn.body.x);
-                    v = value(Xn, goal, terrain);
-                    
-                    % Propogate value to parents
-                    i = obj.rollout_node;
-                    while (i > 0)
-                        % Set node value if greater than previous value
-                        if obj.tree.nodes(i).data.value < v
-                            obj.tree.nodes(i).data.value = v;
-                        end
-                        
-                        % Move to parent
-                        i = obj.tree.nodes(i).parent;
-                    end
-                    
-                    % Start new rollout
-                    obj.rollout_node = obj.tree.randDepth(obj.rollout_depth - 1);
-                end
-                
-                % Expand on current rollout node
-                n = obj.rollout_node;
-                
-                % Generate a set of parameters to try
-                gstate = obj.tree.nodes(n).data.gstate;
-                Xn = obj.tree.nodes(n).data.X;
-                terrain = obj.env.getLocalTerrain(Xn.body.x);
-                [cparams_gen, gstate] = generate_params(Xn, goal, terrain, gstate);
-                obj.tree.nodes(n).data.gstate = gstate;
-                
-                % Simulate a step
-                t_stop = 1 / cparams_gen.phase_rate;
-                [Xp, cstatep] = biped_sim(Xn, obj.tree.nodes(n).data.cstate, ...
-                    obj.robot, cparams_gen, terrain, t_stop, obj.Ts_sim);
-                
-                % Evaluate the result and add child node
-                ss = SimulationState(Xp, cstatep, cparams_gen, GeneratorState(), -inf);
-                c = obj.tree.addChild(n, ss);
-                
-                % If unable to add child node, delete the lowest value child
-                if ~c
-                    v_min = inf;
-                    c_min = uint32(0);
-                    for i = 1:numel(obj.tree.nodes(n).children)
-                        ci = obj.tree.nodes(n).children(i);
-                        if ci
-                            vc = obj.tree.nodes(ci).data.value;
-                            if vc < v_min
-                                v_min = vc;
-                                c_min = ci;
-                            end
-                        end
-                    end
-                    if c_min
-                        obj.tree.deleteNode(c_min);
-                        c = obj.tree.addChild(n, ss);
-                    end
-                end
-                
-                % Child is next parent for the rollout
-                obj.rollout_node = c;
-            end
+%             goal = Goal();
+%             goal.dx = obj.target_dx;
+%             
+%             cparams = obj.tree.nodes(1).data.cparams;
+%             
+%             % Calculate leg phases at next planner timestep
+%             phase_inc = obj.Ts * cparams.phase_rate;
+%             phase_right_next = cstate.phase.right + phase_inc;
+%             phase_left_next = cstate.phase.left + phase_inc;
+%             
+%             % Check whether next planner timestep will start a new step
+%             if phase_right_next >= 1 || phase_left_next >= 1 || ...
+%                     cstate.phase.right == 0 || cstate.phase.left == 0
+%                 % Project one planer timestep forward with current parameters
+%                 terrain = obj.env.getLocalTerrain(X.body.x);
+%                 [Xp, cstatep] = biped_sim(X, cstate, obj.robot, cparams, terrain, obj.Ts, obj.Ts_sim);
+%                 
+%                 % Choose the new parameters with the highest estimated Q value
+%                 v_max = -inf;
+%                 for i = 1:numel(obj.tree.nodes(1).children)
+%                     c = obj.tree.nodes(1).children(i);
+%                     if c
+%                         v = obj.tree.nodes(c).data.value;
+%                         if v > v_max
+%                             cparams = obj.tree.nodes(c).data.cparams;
+%                             v_max = v;
+%                         end
+%                     end
+%                 end
+%                 
+%                 % Simulate until the end of the next step
+%                 phase_next_max = max(mod(phase_right_next, 1), mod(phase_left_next, 1));
+%                 t_stop = (1 - phase_next_max) / cparams.phase_rate;
+%                 terrain = obj.env.getLocalTerrain(Xp.body.x);
+%                 [Xp, cstatep] = biped_sim(Xp, cstatep, obj.robot, cparams, terrain, t_stop, obj.Ts_sim);
+%                 
+%                 % Reset tree with predicted state as root
+%                 terrain = obj.env.getLocalTerrain(Xp.body.x);
+%                 vp = value(Xp, goal, terrain);
+%                 ss = SimulationState(Xp, cstatep, cparams, GeneratorState(), vp);
+%                 obj.tree.reset(ss);
+%                 obj.rollout_node = uint32(1);
+%             else
+%                 % Otherwise, grow the tree
+%                 
+%                 % TODO: only pick nodes with no high-value child
+%                 
+%                 % Check whether max depth on current rollout has been reached
+%                 if obj.tree.nodes(obj.rollout_node).depth >= obj.rollout_depth
+%                     % Evaluate leaf node
+%                     Xn = obj.tree.nodes(obj.rollout_node).data.X;
+%                     terrain = obj.env.getLocalTerrain(Xn.body.x);
+%                     v = value(Xn, goal, terrain);
+%                     
+%                     % Propogate value to parents
+%                     i = obj.rollout_node;
+%                     while (i > 0)
+%                         % Set node value if greater than previous value
+%                         if obj.tree.nodes(i).data.value < v
+%                             obj.tree.nodes(i).data.value = v;
+%                         end
+%                         
+%                         % Move to parent
+%                         i = obj.tree.nodes(i).parent;
+%                     end
+%                     
+%                     % Start new rollout
+%                     obj.rollout_node = obj.tree.randDepth(obj.rollout_depth - 1);
+%                 end
+%                 
+%                 % Expand on current rollout node
+%                 n = obj.rollout_node;
+%                 
+%                 % Generate a set of parameters to try
+%                 gstate = obj.tree.nodes(n).data.gstate;
+%                 Xn = obj.tree.nodes(n).data.X;
+%                 terrain = obj.env.getLocalTerrain(Xn.body.x);
+%                 [cparams_gen, gstate] = generate_params(Xn, goal, terrain, gstate);
+%                 obj.tree.nodes(n).data.gstate = gstate;
+%                 
+%                 % Simulate a step
+%                 t_stop = 1 / cparams_gen.phase_rate;
+%                 [Xp, cstatep] = biped_sim(Xn, obj.tree.nodes(n).data.cstate, ...
+%                     obj.robot, cparams_gen, terrain, t_stop, obj.Ts_sim);
+%                 
+%                 % Evaluate the result and add child node
+%                 ss = SimulationState(Xp, cstatep, cparams_gen, GeneratorState(), -inf);
+%                 c = obj.tree.addChild(n, ss);
+%                 
+%                 % If unable to add child node, delete the lowest value child
+%                 if ~c
+%                     v_min = inf;
+%                     c_min = uint32(0);
+%                     for i = 1:numel(obj.tree.nodes(n).children)
+%                         ci = obj.tree.nodes(n).children(i);
+%                         if ci
+%                             vc = obj.tree.nodes(ci).data.value;
+%                             if vc < v_min
+%                                 v_min = vc;
+%                                 c_min = ci;
+%                             end
+%                         end
+%                     end
+%                     if c_min
+%                         obj.tree.deleteNode(c_min);
+%                         c = obj.tree.addChild(n, ss);
+%                     end
+%                 end
+%                 
+%                 % Child is next parent for the rollout
+%                 obj.rollout_node = c;
+%             end
         end
         
         
@@ -156,6 +158,7 @@ classdef Planner < matlab.System & matlab.system.mixin.Propagates
             obj.tree.reset(SimulationState());
             obj.rollout_node = uint32(1);
             obj.env.ground_data = obj.ground_data;
+            obj.cparams = ControllerParams();
         end
         
         
--- controller_step.m
+++ controller_step.m
@@ -1,155 +1,30 @@
-function [u, s] = controller_step(X, s, p, Ts)
+function [u, c] = controller_step(X, c, p, Ts)
 
 % Get singleturn body angle to allow for flips
 X.body.theta = mod(X.body.theta + pi, 2*pi) - pi;
 
-% Above some speed, increase step rate instead of increasing stride length
-if abs(X.body.dx / p.phase_rate) > p.max_stride * 2
-     p.phase_rate = abs(X.body.dx / p.max_stride / 2);
-end
+% Construct input layer
+input = [X.body.theta; X.body.dx; X.body.dy; X.body.dtheta;
+    X.right.l_eq; X.right.l - X.right.l_eq; X.right.theta_eq; X.right.theta - X.right.theta_eq;
+    X.right.dl_eq; X.right.dl - X.right.dl_eq; X.right.dtheta_eq; X.right.dtheta - X.right.dtheta_eq;
+    X.left.l_eq; X.left.l - X.left.l_eq; X.left.theta_eq; X.left.theta - X.left.theta_eq;
+    X.left.dl_eq; X.left.dl - X.left.dl_eq; X.left.dtheta_eq; X.left.dtheta - X.left.dtheta_eq;
+    sin(2*pi*c.phase.right); cos(2*pi*c.phase.right); sin(2*pi*c.phase.left); cos(2*pi*c.phase.left)];
 
-% Increase leg phases with time
-s.phase.right = s.phase.right + Ts * p.phase_rate;
-s.phase.left  = s.phase.left  + Ts * p.phase_rate;
+% Hidden layer 1
+h1 = max(p.w1 * [input; 1], 0);
 
-% Capture foot positions at phase rollover
-if s.phase.right >= 1
-    s.foot_x_last.right = X.body.x + X.right.l * sin(X.body.theta + X.right.theta);
-end
-if s.phase.left >= 1
-    s.foot_x_last.left = X.body.x + X.left.l * sin(X.body.theta + X.left.theta);
-end
+% Output layer
+output = p.w2 * [h1; 1];
 
-% Limit phases to [0, 1)
-s.phase.right = s.phase.right - floor(s.phase.right);
-s.phase.left  = s.phase.left  - floor(s.phase.left);
-
-% Estimate body acceleration
-body_ddx_est = (X.body.dx - s.body_dx_last) / Ts;
-s.body_ddx = s.body_ddx + p.ddx_filter * (body_ddx_est - s.body_ddx);
-s.body_dx_last = X.body.dx;
-
-% Detect ground contact
-gc.right = clamp((X.right.l_eq - X.right.l) / p.contact_threshold, 0, 1);
-gc.left = clamp((X.left.l_eq - X.left.l) / p.contact_threshold, 0, 1);
-
-% Update leg targets
-foot_extension = X.body.dx * (0.5 / p.phase_rate) + ...
-    0.1 * clamp(X.body.dx - p.target_dx, -0.5, 0.5) + ...
-    0.03 * s.body_ddx;
-if s.phase.right < p.step_lock_phase
-    s.foot_x_target.right = X.body.x + foot_extension;
-end
-if s.phase.left < p.step_lock_phase
-    s.foot_x_target.left = X.body.x + foot_extension;
-end
-
-% Initialize torque struct
+% Process output
 u = Control();
+u.right.l_eq     = 12*output(1);
+u.right.theta_eq = 12*output(2);
+u.left.l_eq      = 12*output(3);
+u.left.theta_eq  = 12*output(4);
 
-% Get PD controllers for x and y foot position (leg angle and length)
-leg_pd.right.y = get_pd(p.y_pd, s.phase.right);
-leg_pd.right.x = get_pd(p.x_pd, s.phase.right);
-leg_pd.left.y  = get_pd(p.y_pd, s.phase.left);
-leg_pd.left.x  = get_pd(p.x_pd, s.phase.left);
+c.phase.right = c.phase.right + Ts * output(5);
+c.phase.left  = c.phase.left  + Ts * output(5);
 
-% Get transformed leg PD output
-u.right = eval_leg_pd(leg_pd.right, X, X.right, p, s.foot_x_last.right, s.foot_x_target.right);
-u.left  = eval_leg_pd(leg_pd.left,  X, X.left,  p, s.foot_x_last.left,  s.foot_x_target.left);
-
-% Modulate angle target control with ground contact
-u.right.theta_eq = (1 - gc.right) * u.right.theta_eq;
-u.left.theta_eq = (1 - gc.left) * u.left.theta_eq;
-
-% Add feedforward terms for weight compensation and energy injection
-u.right.l_eq = u.right.l_eq + ...
-    eval_ff(p.weight_ff, s.phase.right) * p.robot_weight + ...
-    eval_ff(p.energy_ff, s.phase.right) * p.energy_injection;
-u.left.l_eq = u.left.l_eq + ...
-    eval_ff(p.weight_ff, s.phase.left) * p.robot_weight + ...
-    eval_ff(p.energy_ff, s.phase.left) * p.energy_injection;
-
-% Add body angle control, modulated with ground contact
-u.right.theta_eq = u.right.theta_eq - ...
-    gc.right * eval_pd(p.body_angle_pd, s.phase.right, X.body.theta, X.body.dtheta, 0, 0);
-u.left.theta_eq = u.left.theta_eq - ...
-    gc.left * eval_pd(p.body_angle_pd, s.phase.left, X.body.theta, X.body.dtheta, 0, 0);
-
-end
-
-
-function out = eval_ff(tpoints, phase)
-
-i = 2;
-while tpoints(i).phase < phase
-    i = i + 1;
-end
-
-phase_diff = tpoints(i).phase - tpoints(i - 1).phase;
-p = (phase - tpoints(i - 1).phase) / phase_diff;
-
-out = tpoints(i - 1).value + p * (tpoints(i).value - tpoints(i - 1).value);
-end
-
-
-function out = eval_pd(tpoints, phase, x, dx, zero_point, one_point)
-
-i = 2;
-while tpoints(i).phase < phase
-    i = i + 1;
-end
-
-phase_diff = tpoints(i).phase - tpoints(i - 1).phase;
-p = (phase - tpoints(i - 1).phase) / phase_diff;
-
-target = tpoints(i - 1).target + p * (tpoints(i).target - tpoints(i - 1).target);
-dtarget = (tpoints(i).target - tpoints(i - 1).target) / phase_diff;
-kp = tpoints(i - 1).kp + p * (tpoints(i).kp - tpoints(i - 1).kp);
-kd = tpoints(i - 1).kd + p * (tpoints(i).kd - tpoints(i - 1).kd);
-
-scale = one_point - zero_point;
-offset = zero_point;
-
-out = (kp * ((target * scale + offset) - x)) + (kd * (dtarget * scale - dx));
-end
-
-
-function pd = get_pd(tpoints, phase)
-
-i = 2;
-while tpoints(i).phase < phase
-    i = i + 1;
-end
-
-phase_diff = tpoints(i).phase - tpoints(i - 1).phase;
-p = (phase - tpoints(i - 1).phase) / phase_diff;
-
-pd.target = tpoints(i - 1).target + p * (tpoints(i).target - tpoints(i - 1).target);
-pd.dtarget = (tpoints(i).target - tpoints(i - 1).target) / phase_diff;
-pd.kp = tpoints(i - 1).kp + p * (tpoints(i).kp - tpoints(i - 1).kp);
-pd.kd = tpoints(i - 1).kd + p * (tpoints(i).kd - tpoints(i - 1).kd);
-end
-
-
-function u = eval_leg_pd(pd, X, leg, p, x_last, x_target)
-% Get scaled x and y targets
-x = pd.x.target * (x_target - x_last) + x_last - X.body.x;
-dx = pd.x.dtarget * (x_target - x_last) - X.body.dx;
-y = p.l_max - pd.y.target * p.step_height;
-dy = -pd.y.dtarget * p.step_height;
-
-% Transform to polar
-l = sqrt(x^2 + y^2);
-dl = (x*dx + y*dy) / l;
-theta = real(asin(complex(x / l))) - X.body.theta;
-dtheta = (y*dx - x*dy) / l^2 - X.body.dtheta;
-
-% Compute PD controllers
-u.l_eq = pd.y.kp * (l - leg.l_eq) + pd.y.kd * (dl - leg.dl_eq);
-u.theta_eq = pd.x.kp * (theta - leg.theta_eq) + pd.x.kd * (dtheta - leg.dtheta_eq);
-end
-
-
-function out = clamp(x, l, h)
-out = min(max(x, l), h);
 end
