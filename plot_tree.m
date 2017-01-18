7521cbf4173d23633c9846fb500e60ac9f8bb454
Patrick Clary <pclary@outlook.com>
2017-01-18 13:48:03 -0800

    Fixed edge case

--- plot_tree.m
+++ plot_tree.m
@@ -6,6 +6,9 @@ unused = ~[nodes.index];
 % Get list of parents
 p = double([nodes.parent]);
 p = p(~unused);
+for u = find(unused)
+    p(p > u) = p(p > u) - 1;
+end
 
 % Get node values
 data = [nodes.data];
@@ -16,13 +19,6 @@ goal_values = goal_values(~unused);
 path_values = [data.path_value];
 path_values = path_values(~unused);
 
-% Get child number for each node
-cindex = zeros(size(p));
-cindex(1) = 1;
-for i = 2:length(p)
-    cindex(i) = find(nodes(p(i)).children == i, 1);
-end
-
 index = 1:numel(nodes);
 index = index(~unused);
 
@@ -31,8 +27,8 @@ index = index(~unused);
 % Make sure all nodes are drawn at the correct level
 maxdepth = double(max([nodes.depth]));
 yvals = 1 - ((0:maxdepth) + 1) / (maxdepth + 2);
-for i = index
-    y(i) = yvals(nodes(i).depth + 1);
+for i = 1:numel(index)
+    y(i) = yvals(nodes(index(i)).depth + 1);
 end
 
 figure;
@@ -45,7 +41,7 @@ for i = 1:length(p)
     if p(i)
         line([x(p(i)), x(i)], [y(p(i)), y(i)], 'Color', color);
     end
-    text(x(i), y(i), sprintf('%d\n%.2f\n%.2f\n%.2f', cindex(i), path_values(i), stabilities(i), goal_values(i)));
+    text(x(i), y(i), sprintf('%d\n%.2f\n%.2f\n%.2f', index(i), path_values(i), stabilities(i), goal_values(i)));
 end
 
 axis([0 1 0 1]);
