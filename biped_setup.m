d89a74a95848c389a78bc3004d215d72fc59c6d9
Patrick Clary <pclary@outlook.com>
2017-01-19 15:17:52 -0800

    Better settings

--- biped_setup.m
+++ biped_setup.m
@@ -43,7 +43,10 @@ clear ground_x ground_y ground_stiffness ground_damping ground_friction
 
 % Timesteps
 Ts_controller = 1e-3;
-Ts_planner = 1e-3 * 2;
+Ts_planner = 1e-2;
 Ts_dynamics = 1e-3 / 8;
 Ts_visualization = 16e-3;
-Ts_sim = 1e-3;
+Ts_sim = 1e-3 / 4;
+Ts_tree = 0.5;
+
+rollout_depth = 3;
