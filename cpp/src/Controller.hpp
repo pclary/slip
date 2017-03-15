afe370c4b28224841e213c579fba39c88b7b77b9
Patrick Clary <claryp@outlook.com>
2017-03-14 17:45:42 -0700

    Added springs

--- cpp/src/Controller.hpp
+++ cpp/src/Controller.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
 
+struct controller_params_t {
+    double target_dx = 0;
+    double step_offset = 0;
+    double energy_injection = 0;
+    double step_height = 0.1;
+    double phase_rate = 1;
+};
+
+
 struct leg_controller_state_t {
     double phase;
     double foot_x_last;
@@ -16,25 +25,17 @@ struct controller_state_t {
 };
 
 
-struct controller_params_t {
-    double target_dx = 0;
-    double step_offset = 0;
-    double energy_injection = 0;
-    double step_height = 0.1;
-    double phase_rate = 1;
+struct leg_control_t {
+    double angle;
+    double length;
 };
 
 
-// class Controller {
-// public:
-//     controller_params_t cparams;
+struct control_t {
+    leg_control_t right;
+    leg_control_t left;
+};
 
-    
 
-// private:
-//     leg_controller_state_t right = {0, 0, 0};
-//     leg_controller_state_t left = {0.5, 0, 0};
-//     double body_ddx = 0;
-//     double body_dx_last = 0;
 
-// };
+control_t step(robot_state_t X, controller_params_t& cparams, double Ts);
