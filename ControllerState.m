af260130b8fac407aa01b44cb2ff152af6880077
Patrick Clary <pclary@outlook.com>
2017-03-20 14:42:55 -0700

    Improved data collection

--- ControllerState.m
+++ ControllerState.m
@@ -1,10 +1,12 @@
 function cstate = ControllerState()
 
-cstate.right.phase = 0;
+offset = rand() * 0.5;
+
+cstate.right.phase = offset;
 cstate.right.foot_x_last = 0;
 cstate.right.foot_x_target = 0;
 
-cstate.left.phase = 0.5;
+cstate.left.phase = 0.5 + offset;
 cstate.left.foot_x_last = 0;
 cstate.left.foot_x_target = 0;
 
--- gen_data.m
+++ gen_data.m
@@ -12,6 +12,7 @@ end
 npar = 16;
 
 tic
+j = 0;
 
 while true
     
@@ -28,13 +29,19 @@ while true
         if out(i).ErrorMessage
             disp(out(i).ErrorMessage);
         end
-        scores_total = [scores_total; out(i).scores(2:end, :)];
-        rs_total = [rs_total; out(i).rs(2:end, :)];
-        cs_total = [cs_total; out(i).cs(2:end, :)];
-        tr_total = [tr_total; out(i).tr(2:end, :)];
+        scores_total = [scores_total; out(i).scores(4:end-3, :)];
+        rs_total = [rs_total; out(i).rs(4:end-3, :)];
+        cs_total = [cs_total; out(i).cs(4:end-3, :)];
+        tr_total = [tr_total; out(i).tr(4:end-3, :)];
     end
     
     fprintf('%d data points\n', size(scores_total, 1));
     save planner_data.mat scores_total rs_total cs_total tr_total
+    
+    j = j + 1;
+    if j > 50
+        delete(gcp('nocreate'))
+        j = 0;
+    end
 end
 
