0f98879ca945ccd64275b5f427fbe1afb0d7dd93
Patrick Clary <claryp@outlook.com>
2017-03-21 15:36:02 -0700

    Slight tweak to data generation

--- gen_data.m
+++ gen_data.m
@@ -29,19 +29,19 @@ while true
         if out(i).ErrorMessage
             disp(out(i).ErrorMessage);
         end
-        scores_total = [scores_total; out(i).scores(4:end-3, :)];
-        rs_total = [rs_total; out(i).rs(4:end-3, :)];
-        cs_total = [cs_total; out(i).cs(4:end-3, :)];
-        tr_total = [tr_total; out(i).tr(4:end-3, :)];
+        scores_total = [scores_total; out(i).scores(10:end-10, :)];
+        rs_total = [rs_total; out(i).rs(10:end-10, :)];
+        cs_total = [cs_total; out(i).cs(10:end-10, :)];
+        tr_total = [tr_total; out(i).tr(10:end-10, :)];
     end
     
     fprintf('%d data points\n', size(scores_total, 1));
     save planner_data.mat scores_total rs_total cs_total tr_total
     
-%     j = j + 1;
-%     if j > 50
-%         delete(gcp('nocreate'))
-%         j = 0;
-%     end
+    j = j + 1;
+    if j > 50
+        delete(gcp('nocreate'))
+        j = 0;
+    end
 end
 
